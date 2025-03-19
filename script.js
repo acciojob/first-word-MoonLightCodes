5937f4bedd33033d7ce01d71401c23956828352d
MoonLightCodes <poornachander295@gmail.com>
2025-03-19 15:15:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function firstWord(s) {
-  // your code here
-	s.trim();
-	return s.split(' ')[0];
-	
+document.getElementById('btn').addEventListener('click',()=>{
+	document.getElementById('res').textContent=firstWord(document.getElementById('int').value);
+})
+function firstWord(a) {
+	return a.split(' ')[0];
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
